63b60f3a5e9ea3ea7875ecd236ee44bf7c305623
DySeath <sperouffe@gmail.com>
2020-02-01 12:20:54 +0100

    add prefabs and mats

---
+++ Assets/Balloon.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balloon : TestObject
+{
+    public float m_force = 1;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    protected override void FixedUpdate()
+    {
+        if (TestCreate.Instance.IsNavigating)
+        {
+            RigidBody.AddForce(m_force * Vector3.up);
+        }
+    }
+}
