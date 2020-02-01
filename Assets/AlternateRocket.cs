3797ee68eb6e91e516536fbcb54a9c5bb6113ae1
DySeath <sperouffe@gmail.com>
2020-02-01 19:20:10 +0100

    balancing, fix rocket and add alternate rocket

--- Assets/AlternateRocket.cs
+++ Assets/AlternateRocket.cs
@@ -2,9 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rocket : TestObject
+public class AlternateRocket : TestObject
 {
     public SpriteRenderer m_flame = null;
+    public Transform m_flameWrapper = null;     
     public float m_force = 1;
     public bool m_hasFuel = false;
     public float m_fuel = 45;
@@ -20,6 +21,7 @@ public class Rocket : TestObject
     {
         base.Update();
         m_flame.color = (Functionning) ? Color.white : (IsPlacing ? new Color(1,1,1,0.8f) : new Color(0, 0, 0, 0));
+        m_flameWrapper.transform.localScale = (Mathf.Sin(Time.time) + 1) / 2f * new Vector3(1, 1, 1);
 
         if (Functionning && m_hasFuel)
         {
@@ -37,7 +39,7 @@ public class Rocket : TestObject
     {
         if (Functionning)
         {
-            RigidBody.AddRelativeForce(new Vector2(0, m_force));
+            RigidBody.AddRelativeForce(new Vector2(0, m_force * (Mathf.Sin(Time.time) + 1) / 2f));
         }
     }
 }
