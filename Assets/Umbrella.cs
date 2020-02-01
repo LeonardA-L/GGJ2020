aefc78d87d94be92ff2a3f6698f4bf237d858020
LeonardA-L <leonard@allain-launay.fr>
2020-02-01 19:18:25 +0100

    Import umbrella

---
+++ Assets/Umbrella.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Umbrella : TestObject
+{
+    public Animator m_animator = null;
+
+    public override void OnActivate()
+    {
+        base.OnActivate();
+        m_animator.SetTrigger("Activate");
+    }
+    public override void OnDeactivate()
+    {
+        base.OnDeactivate();
+        m_animator.SetTrigger("Deactivate");
+    }
+}
