fb750a700abf17ea7134e36d3e81f7a947b6a76f
LeonardA-L <leonard@allain-launay.fr>
2020-02-02 03:43:41 +0100

    Fix umbrellanimations and umbrella mechanic

--- Assets/Umbrella.cs
+++ Assets/Umbrella.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Umbrella : TestObject
 {
     public Animator m_animator = null;
+    public float m_counterForce = 0.3f;
 
     public override void OnActivate()
     {
@@ -15,5 +16,13 @@ public class Umbrella : TestObject
     {
         base.OnDeactivate();
         m_animator.SetTrigger("Deactivate");
+    }
+    protected override void FixedUpdate()
+    {
+        if (IsActive)
+        {
+            float dot = Vector3.Dot(-RigidBody.velocity.normalized, transform.up.normalized);
+            RigidBody.AddRelativeForce(dot * transform.up.normalized * RigidBody.velocity.magnitude * m_counterForce);
+        }
     }
 }
